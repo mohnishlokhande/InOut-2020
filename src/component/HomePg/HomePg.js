b9225115635b42109e8bdce1f7ad3d64e27d5c58
Mohnish Lokhande <mohnishlokhande14@gmail.com>
2020-12-04 23:32:01 +0530

    start

--- src/component/HomePg/HomePg.js
+++ src/component/HomePg/HomePg.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
+import './HomePg.css';
 
 class Home extends Component{
 
     render(){
         return(
             <div>
-
+                <div className="cardho">
+                    <h3>Welcome To Home Page </h3>&nbsp;
+                    <br/>
+                    <h2>:')</h2>
+                </div>
             </div>
         );
     }
 }
+
+
+export default Home;
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,34 +1,13 @@
 import React, {Component} from 'react';
-import Header from './Header/Header';
-import Footer from './Footer/Footer';
-import Pathways from './Pathways/Pathways';
-import Methodology from './Methodology/Methodology';
-import About  from './About/Aboutus';
-import About2  from './About/Aboutus2';
-import About3  from './About/Aboutus3';
-import About4  from './About/Aboutus4';
-import About5  from './About/Aboutus5';
-import Contact from './Contact/ContactUs';
+import Home from './HomePg/HomePg';
+
 import { Switch,Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
     render(){
         return(
             <div>
-                <Header />
-                <Switch>
-                    <Route exact path="/" component={Pathways} />
-                    <Route path="/pathways" component={Pathways} />
-                    <Route path="/metho" component={Methodology} />
-                    <Route path="/about" component={About} />
-                    <Route path="/about2" component={About2}/>
-                    <Route path="/about3" component={About3}/>
-                    <Route path="/about4" component={About4}/>
-                    <Route path="/about5" component={About5}/>
-                    <Route path="/contactus" component={Contact}/>
-                    <Redirect to="/pathways" />  
-                </Switch>
-                <Footer />
+                <Home/>
             </div>
         );
     }
