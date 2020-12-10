a53f21a42c082f785dc7afaa4148e8b53a495178
Amit <amit13542.ag@gmail.com>
2020-12-10 20:21:51 +0530

    trying to connect

--- server/server.js
+++ server/server.js
@@ -3,8 +3,11 @@ const app = express();
 const bcrypt = require('bcrypt')    // for hashing passwords
 var routes = require('./routes/index'); //requre routes pbject
 var bodyParser = require('body-parser')// require to handle http post
+const cors = require('cors');
+
 app.use(express.urlencoded({ extended: true }));    //for accessing req.body.name
 app.set('view-engine', 'ejs')    //setting view engine to ejs
+app.use(cors());
 
 // var users = []; //to mimic users in database
 app.listen(process.env.port || 3001);       //starting server   
@@ -14,6 +17,11 @@ var con = require("./mysql");
 
 //import routes from route/index.js
 app.use('/', routes);
+
+//creating test api
+var testAPI = require('./routes/testAPI');
+app.use('/testAPI', testAPI);
+
 app.use('/auth', require('./routes/auth'))
 
 console.log('Running at Port 3001');
--- src/App.js
+++ src/App.js
@@ -5,12 +5,27 @@ import { BrowserRouter } from 'react-router-dom';
 
 
 class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
 
+  }
+  callAPI() {
+    fetch("https://localhost:3001/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
   render(){
-    return(
+    return (
       <BrowserRouter>
         <div className="App">
+        
           <Main></Main>
+          <p>{this.state.apiResponse}</p>
         </div>
       </BrowserRouter>
     );
